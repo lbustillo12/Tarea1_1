4261b09b6fe5be7af3a796ac935854f1cf1a4ba0
lbustillo12 <lizy.bustillo@unah.hn>
2025-02-08 09:34:07 -0600

    Ejercicio 1

---
+++ ejercicio1.dart
@@ -0,0 +1,17 @@
+/*
+1. Cree un programa el cual imprima una lista con los primeros 15 números de
+la sucesión de Fibonacci.
+*/
+void main() {
+  List<int> fibonacci = [];
+  int a = 0, b = 1;
+
+  for (int i = 0; i < 15; i++) {
+    fibonacci.add(a);
+    int temp = a;
+    a = b;
+    b = temp + b;
+  }
+
+  print(fibonacci);
+}
